21e10a627d885ea334abb92e41f2fd4731525006
Stephen Caudill <scaudill@gmail.com>
2014-06-20 11:46:57 -0600

    Fix subject for #digit

--- spec/converter/account_number_character_interpreter_spec.rb
+++ spec/converter/account_number_character_interpreter_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../../lib/converter/account_number_character_interpreter'
 
 describe AccountNumberCharacterIntepreter do
-  subject{ described_class.new(char_matrix) }
+  let(:char_intepreter) { described_class.new(char_matrix) }
 
   describe '#digit' do
+    subject { char_intepreter.digit }
     context 'char matrix for 0' do
       let(:char_matrix) do
         [[' ','_',' '],
@@ -11,7 +12,7 @@ describe AccountNumberCharacterIntepreter do
          ['|','_','|']]
       end
       it 'identifies 0' do
-        expect(subject.digit).to eq(0)
+        expect(subject).to eq(0)
       end
     end
     context 'char matrix for 1' do
@@ -21,7 +22,7 @@ describe AccountNumberCharacterIntepreter do
          [' ',' ','|']]
       end
       it 'identifies 1' do
-        expect(subject.digit).to eq(1)
+        expect(subject).to eq(1)
       end
     end
     context 'char matrix for 2' do
@@ -31,7 +32,7 @@ describe AccountNumberCharacterIntepreter do
          ['|','_',' ']]
       end
       it 'identifies 2' do
-        expect(subject.digit).to eq(2)
+        expect(subject).to eq(2)
       end
     end
     context 'char matrix for 3' do
@@ -41,7 +42,7 @@ describe AccountNumberCharacterIntepreter do
          [' ','_','|']]
       end
       it 'identifies 3' do
-        expect(subject.digit).to eq(3)
+        expect(subject).to eq(3)
       end
     end
     context 'char matrix for 4' do
@@ -51,7 +52,7 @@ describe AccountNumberCharacterIntepreter do
          [' ',' ','|']]
       end
       it 'identifies 4' do
-        expect(subject.digit).to eq(4)
+        expect(subject).to eq(4)
       end
     end
     context 'char matrix for 5' do
@@ -61,7 +62,7 @@ describe AccountNumberCharacterIntepreter do
          [' ','_','|']]
       end
       it 'identifies 5' do
-        expect(subject.digit).to eq(5)
+        expect(subject).to eq(5)
       end
     end
     context 'char matrix for 6' do
@@ -71,7 +72,7 @@ describe AccountNumberCharacterIntepreter do
          ['|','_','|']]
       end
       it 'identifies 6' do
-        expect(subject.digit).to eq(6)
+        expect(subject).to eq(6)
       end
     end
     context 'char matrix for 7' do
@@ -81,7 +82,7 @@ describe AccountNumberCharacterIntepreter do
          [' ',' ','|']]
       end
       it 'identifies 7' do
-        expect(subject.digit).to eq(7)
+        expect(subject).to eq(7)
       end
     end
     context 'char matrix for 8' do
@@ -91,7 +92,7 @@ describe AccountNumberCharacterIntepreter do
          ['|','_','|']]
       end
       it 'identifies 8' do
-        expect(subject.digit).to eq(8)
+        expect(subject).to eq(8)
       end
     end
     context 'char matrix for 9' do
@@ -101,7 +102,7 @@ describe AccountNumberCharacterIntepreter do
          [' ','_','|']]
       end
       it 'identifies 9' do
-        expect(subject.digit).to eq(9)
+        expect(subject).to eq(9)
       end
     end
   end
