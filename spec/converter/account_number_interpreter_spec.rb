bf110168fab36790c3bdeb9845c6641f98036e1a
Stephen Caudill <scaudill@gmail.com>
2014-06-20 15:43:13 -0600

    Add pending test cases for corrected_account_number

--- spec/converter/account_number_interpreter_spec.rb
+++ spec/converter/account_number_interpreter_spec.rb
@@ -137,4 +137,140 @@ describe AccountNumberInterpreter do
     end
 
   end
+
+  describe '#corrected_account_number' do
+    subject { interpreter.corrected_account_number }
+    before  { pending 'implement #corrected_account_number' }
+    context 'when the account_number is 111111111' do
+      let(:ocr_string) do
+        '                           ' \
+        '  |  |  |  |  |  |  |  |  |' \
+        '  |  |  |  |  |  |  |  |  |'
+      end
+      it 'corrects to 711111111' do
+        expect(subject).to eq('711111111')
+      end
+    end
+
+
+    context 'when the account_number is 777777777' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        '  |  |  |  |  |  |  |  |  |' \
+        '  |  |  |  |  |  |  |  |  |'
+      end
+      it 'corrects to 777777177' do
+        expect(subject).to eq('777777177')
+      end
+    end
+
+    context 'when the account_number is 2000000000' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        ' _|| || || || || || || || |' \
+        '|_ |_||_||_||_||_||_||_||_|'
+      end
+      it 'corrects to 200800000' do
+        expect(subject).to eq('200800000')
+      end
+    end
+    context 'when the account_number is 333333333' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        ' _| _| _| _| _| _| _| _| _|' \
+        ' _| _| _| _| _| _| _| _| _|'
+      end
+      it 'corrects to 333393333' do
+        expect(subject).to eq('333393333')
+      end
+    end
+
+    context 'when the account_number is 888888888' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        '|_||_||_||_||_||_||_||_||_|' \
+        '|_||_||_||_||_||_||_||_||_|'
+      end
+      it 'specifies an ambiguous match' do
+        expect(subject).to eq(['888886888', '888888880', '888888988'])
+      end
+    end
+
+    context 'when the account_number is 555555555' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        '|_ |_ |_ |_ |_ |_ |_ |_ |_ ' \
+        ' _| _| _| _| _| _| _| _| _|'
+      end
+      it 'specifies an ambiguous match' do
+        expect(subject).to eq(['555655555', '559555555'])
+      end
+    end
+
+    context 'when the account_number is 666666666' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        '|_ |_ |_ |_ |_ |_ |_ |_ |_ ' \
+        '|_||_||_||_||_||_||_||_||_|'
+      end
+      it 'specifies an ambiguous match' do
+        expect(subject).to eq(['666566666', '686666666'])
+      end
+    end
+
+    context 'when the account_number is 999999999' do
+      let(:ocr_string) do
+        ' _  _  _  _  _  _  _  _  _ ' \
+        '|_||_||_||_||_||_||_||_||_|' \
+        ' _| _| _| _| _| _| _| _| _|'
+      end
+      it 'specifies an ambiguous match' do
+        expect(subject).to eq(['899999999', '993999999', '999959999'])
+      end
+    end
+
+    context 'when the account_number is 490067715' do
+      let(:ocr_string) do
+        '    _  _  _  _  _  _     _ ' \
+        '|_||_|| || ||_   |  |  ||_ ' \
+        '  | _||_||_||_|  |  |  | _|'
+      end
+      it 'specifies an ambiguous match' do
+        expect(subject).to eq(['490067115', '490067719', '490867715'])
+      end
+    end
+
+    context 'when the account_number is ?23456789' do
+      let(:ocr_string) do
+        '    _  _     _  _  _  _  _ ' \
+        ' _| _| _||_||_ |_   ||_||_|' \
+        '  ||_  _|  | _||_|  ||_| _|'
+      end
+      it 'corrects an illegible number' do
+        expect(subject).to eq('123456789')
+      end
+    end
+
+    context 'when the account_number is 0?0000051' do
+      let(:ocr_string) do
+        ' _     _  _  _  _  _  _    ' \
+        '| || || || || || || ||_   |' \
+        '|_||_||_||_||_||_||_| _|  |'
+      end
+      it 'corrects an illegible number' do
+        expect(subject).to eq('000000051')
+      end
+    end
+
+    context 'when the account_number is 49086771?' do
+      let(:ocr_string) do
+        '    _  _  _  _  _  _     _ ' \
+        '|_||_|| ||_||_   |  |  | _ ' \
+        '  | _||_||_||_|  |  |  | _|'
+      end
+      it 'corrects an illegible number' do
+        expect(subject).to eq('490867715')
+      end
+    end
+  end
 end
